9aafe2c17cc22bf23ccdf0f0f2312acff2eae40c
Matthew R Marino <mm02971@srcs.loc>
2017-04-05 09:02:42 -0700

    1 more

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
 function convert(num, base) {
 return parseInt(num,10).toString(2);
 }
+function convert(n, base) {
+
+switch (base)
+{
+case 'bin':
+return parseInt(n, 10).toString(2);
+break;
+case 'hex':
+return parseInt(n, 10).toString(16);
+break;
+case 'oct':
+return parseInt(n, 10).toString(8);
+break;
+default:
+return("You messed up not us");
+}
+}
